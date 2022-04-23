886d302c32e41798a35278df20437ed87618a65c
Chai Phonbopit <phonbopit@gmail.com>
2022-04-23 19:03:24 +0700

    finish contract

---
+++ contract/src/lib.rs
@@ -0,0 +1,121 @@
+/*!
+ * Reference: https://github.com/near/near-sdk-rs/blob/master/examples/non-fungible-token/nft/src/lib.rs
+*/
+use near_contract_standards::non_fungible_token::metadata::{
+    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
+};
+use near_contract_standards::non_fungible_token::NonFungibleToken;
+use near_contract_standards::non_fungible_token::{Token, TokenId};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::{
+    env, near_bindgen, require, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+};
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+    tokens: NonFungibleToken,
+    metadata: LazyOption<NFTContractMetadata>,
+}
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    NonFungibleToken,
+    Metadata,
+    TokenMetadata,
+    Enumeration,
+    Approval,
+}
+
+#[near_bindgen]
+impl Contract {
+    /// Initializes the contract owned by `owner_id` with
+    /// default metadata (for example purposes only).
+    #[init]
+    pub fn new_default_meta(owner_id: AccountId) -> Self {
+        Self::new(
+            owner_id,
+            NFTContractMetadata {
+                spec: NFT_METADATA_SPEC.to_string(),
+                name: "NEAR Spring Submission by Chai".to_string(),
+                symbol: "CHAINFT".to_string(),
+                icon: None,
+                base_uri: None,
+                reference: None,
+                reference_hash: None,
+            },
+        )
+    }
+
+    #[init]
+    pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
+        require!(!env::state_exists(), "Already initialized");
+        metadata.assert_valid();
+        let this = Self {
+            tokens: NonFungibleToken::new(
+                StorageKey::NonFungibleToken,
+                owner_id,
+                Some(StorageKey::TokenMetadata),
+                Some(StorageKey::Enumeration),
+                Some(StorageKey::Approval),
+            ),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+        };
+
+        this
+    }
+
+    /// Mint a new token with ID=`token_id` belonging to `token_owner_id`.
+    ///
+    /// Since this example implements metadata, it also requires per-token metadata to be provided
+    /// in this call. `self.tokens.mint` will also require it to be Some, since
+    /// `StorageKey::TokenMetadata` was provided at initialization.
+    ///
+    /// `self.tokens.mint` will enforce `predecessor_account_id` to equal the `owner_id` given in
+    /// initialization call to `new`.
+    #[payable]
+    pub fn nft_mint(
+        &mut self,
+        token_id: TokenId,
+        token_owner_id: AccountId,
+        // _token_metadata: TokenMetadata,
+    ) -> Token {
+        // everyone can mint
+        // assert_eq!(
+        //     env::predecessor_account_id(),
+        //     self.tokens.owner_id,
+        //     "Unauthorized"
+        // );
+
+        // we use default metadata instead of let input from the user.
+        let default_token_metadata = TokenMetadata {
+          title: Some("Chai NFT Collection".into()),
+          description: Some("NEARSpring Submission by Chai collectible".into()),
+          media: Some("https://ipfs.fleek.co/ipfs/bafybeiazzpn2hr7vllxcg5aykle7dn4rlgwzl45gieoftwaivslcerdrxq".to_string()),
+          media_hash: None,
+          copies: Some(1),
+          issued_at: None,
+          expires_at: None,
+          starts_at: None,
+          updated_at: None,
+          extra: None,
+          reference: None,
+          reference_hash: None,
+      };
+
+        self.tokens
+            .internal_mint(token_id, token_owner_id, Some(default_token_metadata))
+    }
+}
+
+near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
+near_contract_standards::impl_non_fungible_token_approval!(Contract, tokens);
+near_contract_standards::impl_non_fungible_token_enumeration!(Contract, tokens);
+
+#[near_bindgen]
+impl NonFungibleTokenMetadataProvider for Contract {
+    fn nft_metadata(&self) -> NFTContractMetadata {
+        self.metadata.get().unwrap()
+    }
+}
